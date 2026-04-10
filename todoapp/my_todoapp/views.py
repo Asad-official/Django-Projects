a693eb95647c209c9f4981dbbffeb96c3d96ad64
Asad Ashraf <asadashraf.official@gmail.com>
2026-04-10 16:17:29 +0500

    User registration: Created user registration Form and register task model in admin pannel

--- todoapp/my_todoapp/views.py
+++ todoapp/my_todoapp/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,redirect
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    return render(request,"index.html")
 def Register(request):
-    return HttpResponse("Register")
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Login')
+    else:
+        form=RegisterForm()
+        
+    return render(request,"register.html",{'form':form})
 
 def login(request):
-    return HttpResponse("Login")
+    return render(request,"login.html")
 def logout(request):
     return HttpResponse("Logout")
 def delete_task(request,id):
