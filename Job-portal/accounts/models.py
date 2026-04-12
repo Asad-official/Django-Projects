ae0a68c0c0e49634b20a142dc2c824b5dedd25e4
Asad Ashraf <asadashraf.official@gmail.com>
2026-04-12 21:05:39 +0500

    custum user model : created a custumuser model to save data as an employee or employer

--- Job-portal/accounts/models.py
+++ Job-portal/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    is_employee=models.BooleanField(default=False)
+    is_employer=models.BooleanField(default=False)
+
+    phone_number=models.CharField(max_length=15,blank=True,null=True)
+    location=models.CharField( max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return self.username
