5b5ec90d2785f4ef120fc2c114fdb436b6cd75b2
Asad Ashraf <asadashraf.official@gmail.com>
2026-04-13 13:58:36 +0500

    Create jobs model

--- Job-portal/jobs/models.py
+++ Job-portal/jobs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Job(models.Model):
+    employer=models.ForeignKey(settings.AUTH_USER_MODEL,on_manager_managed=True,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    location=models.CharField(max_length=100)
+    salary=models.CharField(max_length=100,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
