6cb547e3100bff4d27ac14c868bd5bafaff7c422
Asad Ashraf <asadashraf.official@gmail.com>
2026-04-13 13:42:11 +0500

    Create jobs app and compelete basic functionality

---
+++ Job-portal/jobs/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def home_view(request):
+    context={
+        'user':request.user
+    }
+    return render(request,'jobs/home.html',context)
